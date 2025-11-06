c2b47626eadb38afb419f574724d146db1ea9d3e
Brian <jorgebrianvdv@gmail.com>
2025-11-06 16:05:23 -0500

    feat: agregado archivos python y electron

---
+++ backend/api_model.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class PathModel(BaseModel):
+    path: str
+    message: Optional[str] = None
+
+
+class HelloModel(BaseModel):
+    name: str
+    message: Optional[str] = None
