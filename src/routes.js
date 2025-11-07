71cd4241484675f72bfeb5bfd224b1f5e30dd982
Brian <jorgebrianvdv@gmail.com>
2025-11-07 15:33:24 -0500

    front end borrado

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Cotizaciones = React.lazy(() => import('./views/cotizaciones/CotizacionesPage'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
-  { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/cotizaciones', name: 'Cotizaciones', element: Cotizaciones },
 ]
 
